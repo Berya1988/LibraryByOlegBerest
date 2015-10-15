3343aa9d9090740e3867618d9ef03cdcd7475ae1
Berya1988 <Berest_Oleg@mail.ru>
2015-10-15 14:33:27 +0300

    Project with next unresolved problem: bugs in attempt to edit information by one client while another performs sorting.

---
+++ Server/src/main/java/controller/IdChecker.java
@@ -0,0 +1,36 @@
+package controller;
+
+import org.apache.log4j.Logger;
+
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * Created by Oleg on 11.09.2015.
+ */
+public class IdChecker implements Runnable {
+    private static final Logger log = Logger.getLogger(IdChecker.class);
+    private Date currentTime;
+    private HashMap<Integer, Date> ids;
+
+    public IdChecker(Date date, HashMap<Integer, Date> ids) {
+        this.ids = ids;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i >= 0 ; i++) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                log.error("Threw a InterruptedException in MainClass::MyMethod:", e);
+            }
+            currentTime = new Date();
+            for(HashMap.Entry<Integer, Date> entry: ids.entrySet()) {
+                if (currentTime.getTime() - entry.getValue().getTime() >= 10000) {
+                    ids.remove(entry.getKey());
+                }
+            }
+        }
+    }
+}
